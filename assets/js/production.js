118382c09260bc3321ac3346dbc8ec340782a91b
Prabhav Khandelwal <me@prabhavkhandelwal.com>
2016-05-07 21:16:54 -0700

    Image in work items baseurl linking
    removed the ‘/‘

--- assets/js/production.js
+++ assets/js/production.js
@@ -1,18 +1,1 @@
 // empty file jquery.js
-// app.jsconst canvas = document.getElementById('webgl-contents');
-
-	var main = function() {
-	$('.genericon-menu').click(function() {
-		// $('nav').toggleClass('active');
-		if($('nav').attr('class') === 'active') {
-			$('nav').addClass('close').removeClass('active');
-			$(this).addClass('close').removeClass('active');
-		}
-		else {
-			$('nav').addClass('active').removeClass('close');
-			$(this).addClass('active').removeClass('close');
-		}
-	});
-};
-
-$(document).ready(main);
